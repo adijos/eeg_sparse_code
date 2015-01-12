6f91b999fea99ebe860830a06c467fa84b9177ea
Matthew <mattboggess7@gmail.com>
2015-01-11 17:32:31 -0800

    Delete old files.

--- code/code/S.m
+++
@@ -1,5 +0,0 @@
-% S.m - sparse cost function
-
-function sparse_cost = S(a)
-
-sparse_cost=log(1+a.*a);
--- code/code/Sp.m
+++
@@ -1,5 +0,0 @@
-% Sp.m - derivative of sparse cost
-
-function Sprime = Sp(a)
-
-Sprime = 2*a./(1+a.*a);
