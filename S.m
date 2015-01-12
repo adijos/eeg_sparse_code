356bdf1963d65d6e4a79f6efc1d281216d4c3c0c
Matthew <mattboggess7@gmail.com>
2015-01-11 17:25:36 -0800

    Removed old/irrelevant implementations. Cleaned up repository.

---
+++ S.m
@@ -0,0 +1,5 @@
+% S.m - sparse cost function
+
+function sparse_cost = S(a)
+
+sparse_cost=log(1+a.*a);
---
+++ plot_Phi.m
@@ -0,0 +1,23 @@
+Phi = load('pre_phi.mat');
+Phi = Phi.Phi;
+[N M w] = size(Phi);
+
+for i = 1:N
+    figure(i)
+    for j=1:M;
+        subplot(4,4,j)
+        plot(reshape(Phi(i, j, :), 1, w));
+        xlim([0 500]);
+    end
+    suptitle(strcat('Electrode #', int2str(i)));
+end
+
+% for i = 1:N
+%     figure(i)
+%     for j=1:M;
+%         subplot(4,4,j)
+%         plot(reshape(Phi(j, i, :), 1, w));
+%         xlim([0 500]);
+%     end
+%     suptitle(strcat('Basis #', int2str(i)));
+% end
